83297348ea99f67522ff45802dd53279625c6ceb
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2015-04-03 22:36:46 +0300

    Initial commit

---
+++ redminecharts/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Sviatoslav Abakumov'
+__email__ = 'dust.harvesting@gmail.com'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'CairoSVG',
+    'cssselect',
+    'flask',
+    'lxml',
+    'pygal',
+    'tinycss',
+]
+
+setup(
+    name='redmine-charts-images',
+    version='0.1.0',
+    description="Display meaningful Redmine charts.",
+    long_description=readme,
+    author="Sviatoslav Abakumov",
+    author_email='dust.harvesting@gmail.com',
+    url='https://github.com/perlence/redmine-charts-images',
+    packages=[
+        'redminecharts',
+    ],
+    package_dir={'redminecharts':
+                 'redminecharts'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='redmine-charts-images',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
